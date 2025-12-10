69fa700a95a8a7c5a2390e934bc36d43f4906cae
yant0 <80298071+yant0@users.noreply.github.com.>
2025-12-10 09:16:23 +0700

    [Anita Nurazizah Agussalim] fix debug

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ def edit_student(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
